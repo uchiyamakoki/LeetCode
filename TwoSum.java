38cde7eeff1cb361eed5a44a8959a33d32554edb
momoka <454768497@qq.com>
2017-12-25 21:38:41 +0800

    第一次提交

---
+++ TwoSum.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr={3,2,4};
+        int[] r=twoSum(arr,6);
+        for (int i=0;i<r.length;i++){
+            System.out.println(r[i]);
+        }
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums==null||nums.length<2){
+            return new int[2];
+        }
+        Map<Integer,Integer> map=new HashMap<Integer, Integer>();
+        for (int i=0;i<nums.length;i++){
+            map.put(target-nums[i],i);
+        }
+        for (int i=0;i<nums.length;i++){
+            Integer v=map.get(nums[i]);
+            if (v!=null&&v!=i){
+                return new int[] {i,v};
+            }
+        }
+        return null;
+    }
+
+
+}
